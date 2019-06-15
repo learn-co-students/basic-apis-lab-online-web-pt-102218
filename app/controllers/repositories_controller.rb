c25f516b70e27171ed25f93396706d62c1b5f3a5
alexandercleasby <cleasby.alex@gmail.com>
2019-06-15 10:19:00 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class RepositoriesController < ApplicationController
 
   def search
@@ -5,5 +6,21 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['q']=params['query']
+        req.params['Authorization']='token 698ccc91f782c545aab5648750cda3295b84190f'
+        
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @results = body['items']
+      else
+
+      end
+    rescue => exception
+      
+    end
+    render 'search'
   end
 end
