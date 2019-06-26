77ae2ccd8d0d7216800d1b5a48658b4ccbc4e4e4
Paul Ashour <paul.ashour@gmail.com>
2019-06-26 09:02:36 -0600

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,14 @@
 class RepositoriesController < ApplicationController
 
   def search
-
+    @results = { items: [] }
   end
 
-  def github_search
+  def github_search()
+    q = params[:query]
+    conn = Faraday.new(url: 'https://api.github.com/')
+    response = conn.get '/search/repositories', { q: q }
+    @results = JSON.parse(response.body)
+    render 'search'
   end
 end
